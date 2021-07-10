709e80e9ee63eca1ce8472ddfb7ec7482c8a3e8b
affanhasby <hasbyaffan@gmail.com>
2021-07-10 21:57:06 +0700

    1

--- Homework_week1_day2/alert.js
+++ Homework_week1_day2/alert.js
@@ -3,6 +3,21 @@ alertFunction = () =>{
   }
 
 axios
-  .get("https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json", {})
-  .then(response => console.log(response.data))
+  .get(
+    "https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json", {}
+    )
+  .then(function(response) {
+    console.log(response.data);
+
+    document.getElementById('gambar').src= response.data.album.images[0].url;
+
+    const trackTitleDOM = document.getElementById('judul')
+    trackTitleDOM.innerHTML = `Title : ${response.data.name}`
+
+    const artisDOM = document.getElementById('artis')
+    artisDOM.innerHTML = `Artist : ${response.data.artists[0].name}`
+
+    const albumDOM = document.getElementById('album')
+    albumDOM.innerHTML = `Album : ${response.data.name}`
+  })
   .catch(err => alert("Aduhh!"));
