121f73c835693a0facbb45f3e8a0b55558c42802
affanhasby <hasbyaffan@gmail.com>
2021-07-12 19:43:26 +0700

    update

---
+++ session2/my-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        <label for="fname">SEARCH</label><br/>
+        <input type="text"/>
+        <input type="submit" value="Submit"/>
+      </form>
+      <div>
+        <img src="https://media.giphy.com/media/Vh8pbGX3SGRwFDh3V0/source.gif"></img>
+      </div>
+    </div>
+  );
+}
+
+export default App;
